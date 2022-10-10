af402a7bf394c2a88ce8eaa5dfc236cef3bfaf62
Tommy OnyxOne <tommy@onyxonlinelearning.eu>
2022-10-10 19:31:49 +0000

    now can extract qrcode image out of pdf
    by using "pdfimages" CLI tool - https://askubuntu.com/questions/117143/is-there-a-command-line-tool-to-bulk-extract-images-from-a-pdf

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ include 'vendor/autoload.php';
  
 // Parse pdf file and build necessary objects.
 $parser = new \Smalot\PdfParser\Parser();
-$pdf    = $parser->parseFile('samples/SimpleInvoiceFilledExample1.pdf');
+// $pdf    = $parser->parseFile('samples/SimpleInvoiceFilledExample1.pdf');
+$filename = 'b.pdf';
+$pdf    = $parser->parseFile($filename);
  
 $text = $pdf->getText();
-echo $text;
- 
+echo $text . '\n';
+exec('pdfimages -all ' . $filename . ' output-') ;
+exec('ls output*', $yoyo);
+print_r($yoyo);
 ?>
