ff940f32ab707c9c65e87402d8c719e16f96ce8a
Tommy OnyxOne <tommy@onyxonlinelearning.eu>
2022-10-11 20:45:06 +0000

    (chores) need to use "before" key since apt-get install result is NOT in workspace. Can use "command" too but a terminal will be opened I guess

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include 'vendor/autoload.php';
 // Parse pdf file and build necessary objects.
 $parser = new \Smalot\PdfParser\Parser();
 // $pdf    = $parser->parseFile('samples/SimpleInvoiceFilledExample1.pdf');
-$filename = 'b.pdf';
+$filename = 'a.pdf';
 $pdf    = $parser->parseFile($filename);
  
 $text = $pdf->getText();
