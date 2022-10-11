99b3820582ea0885eb375b2cfb05c43931ac81e8
Tommy OnyxOne <tommy@onyxonlinelearning.eu>
2022-10-11 21:22:56 +0000

    use getPages()[0]->getDataTm() to read coordinate, then use getTextXY to directly get it!

--- index.php
+++ index.php
@@ -6,12 +6,15 @@ include 'vendor/autoload.php';
 // Parse pdf file and build necessary objects.
 $parser = new \Smalot\PdfParser\Parser();
 // $pdf    = $parser->parseFile('samples/SimpleInvoiceFilledExample1.pdf');
-$filename = 'a.pdf';
+$filename = 'b.pdf';
 $pdf    = $parser->parseFile($filename);
+$text= $pdf->getPages()[0]->getTextXY(null, 718.47);
  
-$text = $pdf->getText();
-echo $text . '\n';
-exec('pdfimages -all ' . $filename . ' output-') ;
-exec('ls output*', $yoyo);
-print_r($yoyo);
+// $text = $pdf->getText();
+// $text = $pdf->getPages()[0]->getDataTm();
+print_r($text);
+// . '\n';
+// exec('pdfimages -all ' . $filename . ' output-') ;
+// exec('ls output*', $yoyo);
+// print_r($yoyo);
 ?>
